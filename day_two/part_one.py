f3eef51f2f52742419e571a4b70a0b21db0a2957
Gabriel Caldas <gcaldasl2000@gmail.com>
2024-12-08 10:10:47 -0300

    day two part 1

---
+++ day_two/part_one.py
@@ -0,0 +1,27 @@
+def check_if_each_sequence_is_valid(sequence):
+    if not sequence or len(sequence) == 1:
+        return True
+
+    is_increasing = sequence[0] < sequence[1]
+
+    for i in range(len(sequence) - 1):
+        diff = sequence[i + 1] - sequence[i]
+
+        if not (0 < abs(diff) <= 3):
+            return False
+
+        if is_increasing and sequence[i] >= sequence[i + 1]:
+            return False
+        elif not is_increasing and sequence[i] <= sequence[i + 1]:
+            return False
+
+    return True
+
+def check_red_nose_reactor(input_file):
+  with open(input_file, 'r') as f:
+    lines = [line.strip() for line in f.readlines()]
+    array_of_nums = [list(map(int, line.split())) for line in lines]
+
+  return sum(1 for seq in array_of_nums if check_if_each_sequence_is_valid(seq))
+
+print(check_red_nose_reactor('input.txt'))
